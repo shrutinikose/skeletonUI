3683739d0176ea35b31dc14d49616dcb71a5db28
Shruti Nikose <shrutinikose@Shrutis-MacBook-Air.local>
2024-07-27 13:45:28 +0530

    skeleton UI

---
+++ src/components/Articles.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import SkeletonElements from '../skeletons/SkeletonElements';
+import SkeletonArticles from '../skeletons/SkeletonArticles';
+
+const Articles = () => {
+    const [articles, setArticles] = useState(null);
+    useEffect(() => {
+        setTimeout(async () => {
+            const result = await fetch('https://jsonplaceholder.typicode.com/posts');
+            const data = await result.json();
+            setArticles(data);
+        }, 5000)
+    })
+    return (
+        <div className='articles'>
+            <h2>Articles</h2>
+            {articles && articles.map(articles => (
+                <div className='article' key={articles.id}>
+                    <h3>{ articles.title }</h3>
+                    <o>{ articles.body }</o>
+                </div>
+            ))}
+            {!articles && [1,2,3,4,5].map(item => <SkeletonArticles theme='light' />)}
+        </div>
+    )
+}
+export default Articles;
---
+++ src/skeletons/SkeletonArticles.js
@@ -0,0 +1,20 @@
+import React from "react";
+import SkeletonElements from "./SkeletonElements";
+import Shimmer from "./Shimmer";
+
+const SkeletonArticles = ({ theme }) => {
+    const themeClass = theme || 'light'
+    return (
+        <div className={`skeleton-wrapper ${themeClass}`}>
+            <div className="skeleton-article">
+                <SkeletonElements type='title' />
+                <SkeletonElements type='text' />
+                <SkeletonElements type='text' />
+                <SkeletonElements type='text' />
+            </div>
+            <Shimmer />
+        </div>
+    )
+};
+
+export default SkeletonArticles;
